fb0bbf81295631baed0108bf8984ee6d85dadf6d
tonis <tanton@paysure.solutions>
2024-03-31 22:00:38 +0300

    Add project files

---
+++ examples/pbr/shaders/build_shader.sh
@@ -0,0 +1,5 @@
+glslangValidator -V ./gbuffer.vert -o ./gbuffer_vert.spv
+glslangValidator -V ./gbuffer.frag -o ./gbuffer_frag.spv
+
+glslangValidator -V ./forward.vert -o ./forward_vert.spv
+glslangValidator -V ./forward.frag -o ./forward_frag.spv
